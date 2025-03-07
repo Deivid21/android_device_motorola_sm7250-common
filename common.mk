56bbae996f860c7d0db8a2bf8cf1d8df2d51ddef
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-07 16:17:48 -0300

    sm7250-common: Switch to Evolution-X
    Change-Id: I1672e8e67198fa4e43b0a67e88ee20444c7895e4

--- common.mk
+++ common.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# GApps
+  WITH_GMS := true
+  TARGET_USES_MINI_GAPPS := true
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
@@ -281,9 +285,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml
-
-# MindTheGapps
- $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 # Moto hardware
 PRODUCT_PACKAGES += \
