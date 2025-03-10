2a2b88eab88c2cc40b440dc7a78d18940d6e098f
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-10 09:26:34 -0300

    sm7250-common: Build MindTheGapps
    Change-Id: I18c8ad8df7b1de2e95678094b490a625fe9cdd19

--- common.mk
+++ common.mk
@@ -326,6 +326,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml
 
+# MindTheGapps
+ $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Misc
 PRODUCT_PACKAGES += \
     libchrome \
